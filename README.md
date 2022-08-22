6c0b74a257409d4236971437f1d72bb9878af4bc
Daniel Sánchez (elordenador) <54474838+danilacasito@users.noreply.github.com>
2022-08-22 12:57:17 +0200

    simplificar API y arreglar bugs

---
+++ README.md
@@ -0,0 +1,4 @@
+# PTERODACTYL-API
+this is a python library designed with interact with api v1 of Pterodactyl.io panel
+
+is not finished yet, but it will be compatible with python
--- pterodactyl_api/client.py
+++ pterodactyl_api/client.py
@@ -1,4 +1,14 @@
 import requests
+class server:
+    def __init__(self):
+        self.allocation = []
+    def setName(self,name):
+        self.name = name
+    def isOwner(self,boolean):
+        self.owner = boolean
+    def addPORT(self,port):
+        self.allocation.append(port)
+
 class Api:
     def __init__(self,url,authkey):
         self.url = url
@@ -13,4 +23,14 @@ class Api:
             self.result = response.json()
         except:
             raise Exception(response.text)
-        return self.result
+        lista = []
+        for i in self.result["data"]:
+            if i["object"] == "server":
+                temp = server()
+                temp.setName(i["attributes"]["name"])
+                temp.isOwner(i["attributes"]["server_owner"])
+                for e in i["attributes"]["relationships"]["allocations"]["data"]:
+                    temp.addPORT(e["attributes"]["port"])
+                lista.append(temp)
+        return lista
+
